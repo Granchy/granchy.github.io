18e4483b0eacee6d379d24df0fbe07843cbe5da4
Granchy <justichelli@gmail.com>
2026-02-16 22:46:44 -0500

    Updates

---
+++ assets/About-BhBwHnv9.js
@@ -0,0 +1,1 @@
+import{j as s,H as e}from"./index-DPAG5Dkt.js";function r(){return s.jsxs("div",{children:[s.jsx(e,{}),s.jsx("h1",{children:"About"}),s.jsx("p",{children:"This is the about page."})]})}export{r as default};
